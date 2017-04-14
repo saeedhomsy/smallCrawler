31912c4e2ae1d9d2a0685cdd70c32edd0d414d87
saeedhomsy <homsysaeed@gmail.com>
2017-04-14 16:50:07 +0300

    add title

---
+++ TenderCrawler/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TenderCrawler.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Data</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:TemplateField HeaderText="Order">
+                        <ItemTemplate>
+                            <%#: Eval("Order") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Classify" ItemStyle-Width="300px">
+                        <ItemTemplate>
+                            <%#: Eval("Classify") %>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Link" ItemStyle-Width="200px">
+                        <ItemTemplate>
+                            <a href='<%#: Eval("Link") %>'>Trend Link Details</a>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TenderCrawler/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace TenderCrawler
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getDat();
+        }
+
+
+        private void getDat()
+        {
+            DataTable DT = new DataTable();
+            int OrderCounter = 1;
+            DT.Columns.Add("Order");
+            DT.Columns.Add("Classify");
+            DT.Columns.Add("Link");
+            DataRow[] drArr = new DataRow[10];
+            int mcCollCount = 10;
+            int pageNum = 10;
+            string pageChar = "n";
+            while (mcCollCount != 0)
+            {
+                string link = "http://www.medicaltenders.com/search.php?total=141&off=" + pageNum.ToString() + "&inc=" + pageChar + "n&global=1&region_name[]=EG&notice_type_new[]=1,2,3,7,10,11,16,9,4,8,5&sector=18&deadline=";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                string resText = sr.ReadToEnd();
+                Regex r = new Regex(@"<td class=""style5"" style=""width:70%; vertical-align:top; text-align:left;"" >.*?</td>");
+                Regex tendLinkReg = new Regex(@"href=""auth.*?""");
+                MatchCollection ms = r.Matches(resText);
+                MatchCollection mas = tendLinkReg.Matches(resText);
+                mcCollCount = ms.Count;
+                var list = ms.Cast<Match>().Select(match => match.Value).ToList();
+                var newlist = mas.Cast<Match>().Select(match => match.Value).ToList();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    DataRow row = DT.NewRow();
+                    row["Order"] = OrderCounter;
+
+                    //####################################
+                    //here can get all key words for our claaify and get it from site category
+                    //####################################
+                    row["Classify"] = (list[i].IndexOf("Pharmaceuticals") > -1 ? "Pharmaceutical" : list[i].IndexOf("Healthcare Equipment and Services") > -1 ? "Medical Equipment" : list[i].IndexOf("Laboratory Equipment and Services") > -1 ? "Laboratory Product" : "Other");
+                    row["Link"] = "http://www.medicaltenders.com/" + newlist[i].Substring(6, newlist[i].Length - 1 - 6);
+                    DT.Rows.Add(row);
+                    OrderCounter++;
+                }
+                pageNum += 10;
+                pageChar = "y";
+            }
+            GridView1.DataSource = DT;
+            GridView1.DataBind();
+        }
+    }
+}
